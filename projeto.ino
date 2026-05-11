e26c12180715fc86d1e4ca95cd4ad70d57277e08
Enzio Corsi <enziosantos99@gmail.com>
2026-05-11 11:49:32 -0300

    menu inicial com troca de musicas no lcd, botoes de cima e baixo ja realiznado as trocas

--- projeto.ino
+++ projeto.ino
@@ -13,6 +13,25 @@ LiquidCrystal lcd(9, 10, 11, 12, 13, A0);
 enum EstadoSistema { MENU, TOCANDO, PAUSADO };
 EstadoSistema estadoAtual = MENU;
 
+const int totalMusicas = 5;
+String nomesMusicas[totalMusicas] = {
+  "Cai Cai Balao",
+  "Brilha Brilha",
+  "Dona Aranha",
+  "Tema do Gas",
+  "Ciranda"
+};
+
+int musicaSelecionada = 0;
+
+int leituraAnteriorCima = HIGH;
+int leituraAnteriorBaixo = HIGH;
+
+unsigned long ultimoCliqueCima = 0;
+unsigned long ultimoCliqueBaixo = 0;
+
+const unsigned long debounceMs = 180;
+
 void atualizarLeds() {
   if (estadoAtual == TOCANDO) {
     digitalWrite(LED_TOCANDO, HIGH);
@@ -26,12 +45,24 @@ void atualizarLeds() {
   }
 }
 
-void atualizarTelaInicial() {
+void atualizarTela() {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Projeto Musica");
+  lcd.print("Selecione:");
   lcd.setCursor(0, 1);
-  lcd.print("Iniciando...");
+  lcd.print(nomesMusicas[musicaSelecionada]);
+}
+
+void proximaMusica() {
+  musicaSelecionada++;
+  if (musicaSelecionada >= totalMusicas) musicaSelecionada = 0;
+  atualizarTela();
+}
+
+void musicaAnterior() {
+  musicaSelecionada--;
+  if (musicaSelecionada < 0) musicaSelecionada = totalMusicas - 1;
+  atualizarTela();
 }
 
 void setup() {
@@ -47,8 +78,24 @@ void setup() {
   pinMode(LED_PAUSADO, OUTPUT);
 
   atualizarLeds();
-  atualizarTelaInicial();
+  atualizarTela();
 }
 
 void loop() {
+  int leituraCima = digitalRead(BOTAO_CIMA);
+  int leituraBaixo = digitalRead(BOTAO_BAIXO);
+  unsigned long agora = millis();
+
+  if (leituraAnteriorCima == HIGH && leituraCima == LOW && (agora - ultimoCliqueCima > debounceMs)) {
+    ultimoCliqueCima = agora;
+    if (estadoAtual == MENU) proximaMusica();
+  }
+
+  if (leituraAnteriorBaixo == HIGH && leituraBaixo == LOW && (agora - ultimoCliqueBaixo > debounceMs)) {
+    ultimoCliqueBaixo = agora;
+    if (estadoAtual == MENU) musicaAnterior();
+  }
+
+  leituraAnteriorCima = leituraCima;
+  leituraAnteriorBaixo = leituraBaixo;
 }
