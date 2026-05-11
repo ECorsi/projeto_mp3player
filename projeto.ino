651b8b7ad4fd158821e22c1cd5f22df7697b2095
Enzio Corsi <enziosantos99@gmail.com>
2026-05-11 14:25:23 -0300

    primeira musica em vetor com reproducao nota por nota

--- projeto.ino
+++ projeto.ino
@@ -13,6 +13,13 @@ LiquidCrystal lcd(9, 10, 11, 12, 13, A0);
 enum EstadoSistema { MENU, TOCANDO, PAUSADO };
 EstadoSistema estadoAtual = MENU;
 
+#define DO4   262
+#define RE4   294
+#define MI4   330
+#define FA4   349
+#define SOL4  392
+#define LA4   440
+
 const int totalMusicas = 5;
 String nomesMusicas[totalMusicas] = {
   "Cai Cai Balao",
@@ -23,7 +30,17 @@ String nomesMusicas[totalMusicas] = {
 };
 
 int musicaSelecionada = 0;
-int frequenciaTeste = 440;
+
+int notas[] = { SOL4, SOL4, LA4, SOL4, MI4, SOL4, LA4, SOL4, MI4, RE4, MI4, DO4 };
+int tempos[] = { 4, 4, 4, 4, 2, 4, 4, 4, 2, 4, 4, 2 };
+
+int notaAtualIndex = 0;
+int totalNotasDaMusica = sizeof(notas) / sizeof(notas[0]);
+unsigned int frequenciaAtual = 0;
+
+unsigned long inicioNotaMs = 0;
+unsigned long duracaoNotaMs = 0;
+unsigned long restoNotaMs = 0;
 
 int leituraAnteriorCima = HIGH;
 int leituraAnteriorBaixo = HIGH;
@@ -83,14 +100,48 @@ void musicaAnterior() {
   atualizarTela();
 }
 
-void iniciarMusica() {
-  estadoAtual = TOCANDO;
-  tone(PINO_BUZZER, frequenciaTeste);
+void voltarParaMenu() {
+  estadoAtual = MENU;
+  noTone(PINO_BUZZER);
+  notaAtualIndex = 0;
+  frequenciaAtual = 0;
+  duracaoNotaMs = 0;
+  restoNotaMs = 0;
   atualizarLeds();
   atualizarTela();
 }
 
+void tocarNotaAtual() {
+  if (notaAtualIndex >= totalNotasDaMusica) {
+    voltarParaMenu();
+    return;
+  }
+
+  frequenciaAtual = notas[notaAtualIndex];
+  byte divisorTempo = tempos[notaAtualIndex];
+
+  duracaoNotaMs = 1000UL / divisorTempo;
+  restoNotaMs = duracaoNotaMs;
+
+  if (frequenciaAtual == 0) noTone(PINO_BUZZER);
+  else tone(PINO_BUZZER, frequenciaAtual);
+
+  inicioNotaMs = millis();
+}
+
+void iniciarMusica() {
+  estadoAtual = TOCANDO;
+  notaAtualIndex = 0;
+  atualizarLeds();
+  atualizarTela();
+  tocarNotaAtual();
+}
+
 void pausarMusica() {
+  unsigned long tempoGasto = millis() - inicioNotaMs;
+  if (tempoGasto < duracaoNotaMs) restoNotaMs = duracaoNotaMs - tempoGasto;
+  else restoNotaMs = 1;
+
   estadoAtual = PAUSADO;
   noTone(PINO_BUZZER);
   atualizarLeds();
@@ -99,14 +150,12 @@ void pausarMusica() {
 
 void retomarMusica() {
   estadoAtual = TOCANDO;
-  tone(PINO_BUZZER, frequenciaTeste);
-  atualizarLeds();
-  atualizarTela();
-}
+  duracaoNotaMs = restoNotaMs;
 
-void voltarParaMenu() {
-  estadoAtual = MENU;
-  noTone(PINO_BUZZER);
+  if (frequenciaAtual == 0) noTone(PINO_BUZZER);
+  else tone(PINO_BUZZER, frequenciaAtual);
+
+  inicioNotaMs = millis();
   atualizarLeds();
   atualizarTela();
 }
@@ -156,6 +205,13 @@ void loop() {
   if (leituraAnteriorStop == HIGH && leituraStop == LOW && (agora - ultimoCliqueStop > debounceMs)) {
     ultimoCliqueStop = agora;
     if (estadoAtual == TOCANDO || estadoAtual == PAUSADO) voltarParaMenu();
+  }
+
+  if (estadoAtual == TOCANDO) {
+    if (millis() - inicioNotaMs >= duracaoNotaMs) {
+      notaAtualIndex++;
+      tocarNotaAtual();
+    }
   }
 
   leituraAnteriorCima = leituraCima;
